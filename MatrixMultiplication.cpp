1c2854610ab2e3be9e60bed3913b9a3e30a2f936
Prabhat Chowdhary <85453914+PrabhatChowdhary@users.noreply.github.com>
2023-05-21 00:21:35 +0530

    function implemented calc MatrixMultiplication.cpp
    Matrix multiplication function called and Output saved in functions local vector matOutput.

--- MatrixMultiplication.cpp
+++ MatrixMultiplication.cpp
@@ -11,41 +11,27 @@ using namespace std;
 	//
 
 */
-int main() {
 
+void matrixMult(vector<int>* mat1,vector<int>* mat2) {
 	vector<vector<int>> matOutput;
 	int oneElement = 0;
 	int x = 0;
 	int y = 0;
-	
-	std::vector<std::vector<int>> mat1{
-		{1 , 2, 3},
-		{10, 20, 30},
-		{100, 200, 300}
-	};
-	
-	std::vector<std::vector<int>> mat2{
-		{1 , 2, 3},
-		{10, 20, 30},
-		{100, 200, 300}
-	};
-
-	std::cout << "dimensions of matrix 2 are " << mat2[0].size()<<" x "<<mat2.size() << std::endl;
-	
-	//function loop starts here
+	cout << "element 1,2 of mat1 is " << mat1[1][1] << endl;
 	for (x = 0; x < 3; x++) {
 		vector<int> matOutput_row;
 		for (y = 0; y < 3; y++) {
 			for (int j = 0; j < 3; j++) {
 				oneElement = oneElement + (mat1[x][j] * mat2[j][y]);
-				cout << "input elements are - mat1 " << mat1[x][j] << " and for mat2 " << mat2[j][y]<<endl;
+				cout << "input elements are - mat1 " << mat1[x][j] << " and for mat2 " << mat2[j][y] << endl;
 			}
-			matOutput_row.push_back(oneElement) ;
+			matOutput_row.push_back(oneElement);
 			oneElement = 0;
 		}
-		matOutput.push_back(matOutput_row) ;
+		matOutput.push_back(matOutput_row);
 
 	}
+
 	cout << "the output of multiplied matrix is " << endl;
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
@@ -53,6 +39,28 @@ int main() {
 		}
 		cout << endl;
 	}
-	
+
+}
+
+int main() {
+	std::vector<std::vector<int>> OutputMatrix;
+
+	std::vector<std::vector<int>> mat1{
+		{1 , 2, 3},
+		{10, 20, 30},
+		{100, 200, 300}
+	};
+
+	std::vector<std::vector<int>> mat2{
+		{1 , 2, 3},
+		{10, 20, 30},
+		{100, 200, 300}
+	};
+
+	std::cout << "dimensions of matrix 2 are " << mat2[0].size() << " x " << mat2.size() << std::endl;
+
+	//function call for matrix multiplication
+	matrixMult(mat1.data(), mat2.data());
+
 	return 0;
 }
