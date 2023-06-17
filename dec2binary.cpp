dc933fe8ccdfceea261854071dd24fd066c43ee0
Prabhat Chowdhary <85453914+PrabhatChowdhary@users.noreply.github.com>
2023-06-17 13:59:20 +0530

    Update dec2binary.cpp

--- dec2binary.cpp
+++ dec2binary.cpp
@@ -3,26 +3,31 @@
 #include <vector>
 using namespace std;
 
-
-int main() {
-    int a=151;
+vector<int> dec2bin(int a){
     vector<int> a_bin;
     
     while (a>0){
-        a_bin.push_back(a%2);
-        a=a/2;
+    a_bin.push_back(a%2);
+    a=a/2;
     }
     
-    
-    for(int i=0;i<a_bin.size()/2;i++){
+     for(int i=0;i<a_bin.size()/2;i++){
         swap(a_bin[i],a_bin[(a_bin.size()-1)-i]);
-        //cout<<a_bin[i];
-    }
+        }
+    
+    return a_bin;
+}
+
+int main() {
+    int a=12;
+    vector<int> a_bin;
+    
+   a_bin=dec2bin(a);
     
     for(int i=0;i<a_bin.size();i++){
+        
         cout<<a_bin[i];
     }
     
-    
-    return 0;
+     return 0;
 }
