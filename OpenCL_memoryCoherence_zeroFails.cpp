27838ac7a7068f775f8bad1ab650a6afc545a561
Prabhat Chowdhary <85453914+PrabhatChowdhary@users.noreply.github.com>
2023-07-16 17:23:48 +0530

    Create OpenCL_memoryCoherence_zeroFails.cpp
    There will be zero memory coherence failures here.
    This is because memory barrier is added using barrier(CLK_GLOBAL_MEM_FENCE) before accessing otherValue in the OpenCL kernel. This barrier ensures memory synchronization and coherence before the read operation, preventing memory coherence issues.

---
+++ OpenCL_memoryCoherence_zeroFails.cpp
@@ -0,0 +1,86 @@
+#define CL_HPP_ENABLE_EXCEPTIONS
+#define CL_HPP_TARGET_OPENCL_VERSION 200
+
+#include <CL/opencl.hpp>
+#include <iostream>
+#include <vector>
+
+const size_t n = 1000;  // Array size
+
+int main() {
+    std::vector<int> data(n);
+
+    // Initialize the data vector with an alternating pattern
+    for (int i = 0; i < n; i++) {
+        data[i] = i % 2;  // Alternating pattern of 0 and 1
+    }
+
+    try {
+        // Get available platforms
+        std::vector<cl::Platform> platforms;
+        cl::Platform::get(&platforms);
+
+        // Get available devices
+        std::vector<cl::Device> devices;
+        platforms[0].getDevices(CL_DEVICE_TYPE_ALL, &devices);
+
+        // Create a context and command queue
+        cl::Context context(devices);
+        cl::CommandQueue queue(context, devices[0]);
+
+        // Create the buffer
+        cl::Buffer buffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(int) * n, data.data());
+
+        // Read the OpenCL kernel code from a string
+        std::string kernel_code = R"(
+            __kernel void test_coherency(__global int* buffer) {
+                int id = get_global_id(0);
+                int value = buffer[id];
+                barrier(CLK_GLOBAL_MEM_FENCE);
+                int otherValue = buffer[(id + 1) % get_global_size(0)];
+                buffer[id] = value + otherValue;
+            }
+        )";
+
+        // Create the program and build it
+        cl::Program program(context, kernel_code);
+        program.build(devices);
+
+        // Create the kernel
+        cl::Kernel kernel(program, "test_coherency");
+
+        // Set the kernel argument
+        kernel.setArg(0, buffer);
+
+        // Enqueue the kernel
+        cl::NDRange globalSize(n);
+        queue.enqueueNDRangeKernel(kernel, cl::NullRange, globalSize, cl::NullRange);
+
+        // Read the modified data back to the host
+        queue.enqueueReadBuffer(buffer, CL_TRUE, 0, sizeof(int) * n, data.data());
+
+        // Verify the results and count passing and failing cases
+        int passingCount = 0;
+        int failingCount = 0;
+        for (int i = 0; i < n; i++) {
+            int expected = (i % 2) + ((i + 1) % 2);  // Expected value after the read-modify-write operation
+            if (data[i] == expected) {
+                passingCount++;
+            }
+            else {
+                failingCount++;
+            }
+        }
+
+        // Print the passing and failing counts
+        std::cout << "Passing Cases: " << passingCount << std::endl;
+        std::cout << "Failing Cases: " << failingCount << std::endl;
+
+    }
+    catch (cl::Error& err) {
+        std::cerr << "OpenCL error: " << err.what() << " (" << err.err() << ")" << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
