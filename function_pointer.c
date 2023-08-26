fdcb986c9d075ed39197dacf0cab5113754b5b19
Prabhat Chowdhary <85453914+PrabhatChowdhary@users.noreply.github.com>
2023-08-26 18:29:02 +0530

    Update function_pointer.c

--- function_pointer.c
+++ function_pointer.c
@@ -8,6 +8,21 @@ int sub(int x,int y){
     return x-y;
 }
 
+void wrapper(void (*fun_w)(int x), int x){
+    fun_w(x);
+}
+
+void fun(int a){
+    printf("value of a is %d\n", a);
+}
+
+void print(void * val){
+    
+    printf("value is %d",*(int*)(val+ 2*sizeof(int)));//we use index multiplied by size of int since val is a void *
+}
+
+
+
 int main() {
     // Write C code here
     add(2,3);
@@ -19,5 +34,15 @@ int main() {
     //declaring a pointer for sub funtion
     int (*sub_ptr)(int, int) = &sub;
     printf("%d\n",(*sub_ptr)(44,11));
+
+    void (*fun_ptr)(int) = &fun;
+    (*fun_ptr)(2);
+    fun(4);
+    wrapper(fun,12);
+    wrapper(*fun_ptr,22);
+    
+    int arr[10]={22,33,44,11,44,55,66,77,99,10};
+   printf("%d\n",*(arr+2));//we can directly use index because arr is an int pointer
+    print(arr);
     return 0;
 }
