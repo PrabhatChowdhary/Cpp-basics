d0bb793a6957c16578199ad754a2a4eaf27a753c
Prabhat Chowdhary <85453914+PrabhatChowdhary@users.noreply.github.com>
2024-01-27 13:42:20 +0530

    Update OpenCL_memoryCoherence_zeroFails.cpp

--- OpenCL_memoryCoherence_zeroFails.cpp
+++ OpenCL_memoryCoherence_zeroFails.cpp
@@ -29,7 +29,7 @@ int main() {
         cl::CommandQueue queue(context, devices[0]);
 
         // Create the buffer
-        cl::Buffer buffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(int) * n, data.data());
+        cl::Buffer buffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(int) * n, data);
 
         // Read the OpenCL kernel code from a string
         std::string kernel_code = R"(
